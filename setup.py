193464126531d63b8af1d91e99112a24a30c3105
Arya Wiratama <aryawiratama2401@gmail.com>
2025-03-24 21:33:55 +0700

    update documentation on pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as file:
+    long_des = file.read()
+
 setup(
     name="fakedpy",
-    version="1.0",
+    version="1.1",
     packages=find_packages(),
     package_data={"fakedpy": ["*.py"]},
     install_requires=[
@@ -10,6 +13,8 @@ setup(
         'faker'
     ],
     description="A simple fake data generator that exports results to CSV",
+    long_description=long_des,
+    long_description_content_type="text/markdown",
     author="aryawiratama2401@gmail.com",
     python_requires='>=3.10'
 )
